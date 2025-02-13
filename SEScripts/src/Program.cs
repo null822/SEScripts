0940bee462f55b655d2b11f48001a78b193c3aa3
null8 <noah.klikauer@outlook.com>
2025-02-13 14:00:03 +1100

    Initial commit

---
+++ SEScripts/src/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SEScripts;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var n1 = double.Parse(Console.ReadLine() ?? "");
+        var n2 = double.Parse(Console.ReadLine() ?? "");
+        var n3 = double.Parse(Console.ReadLine() ?? "");
+        
+        Console.WriteLine(n1 + n2 - n3);
+    }
+}
